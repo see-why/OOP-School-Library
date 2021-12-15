6c91ec8c3593ad40b00399da3fca1a8649a51214
see-why <iyadicy@gmail.com>
2021-12-15 23:33:49 +0100

    Add rentals relationship

--- student.rb
+++ student.rb
@@ -6,6 +6,13 @@ class Student < Person
     @classroom = classroom
   end
 
+  attr_reader :classroom
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
   def play_hooky
     "¯\(ツ)/¯"
   end
