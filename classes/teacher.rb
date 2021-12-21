cd09eee2714ecaee9eec6d7df93947390cb0e495
see-why <iyadicy@gmail.com>
2021-12-21 19:56:21 +0100

    Add get attribute for specialization property

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -2,6 +2,7 @@ require_relative './person'
 
 class Teacher < Person
   attr_reader :specialization
+
   def initialize(age, specialization, name = 'Unknown')
     super(age, name, parent_permission: true)
     @specialization = specialization
