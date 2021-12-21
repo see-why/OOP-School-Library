9289c8b5a5afc7c120cc478e551bebeb538a4e17
see-why <iyadicy@gmail.com>
2021-12-21 19:56:45 +0100

    Remove read/write functions

--- modules/appfunctions.rb
+++ modules/appfunctions.rb
@@ -2,9 +2,11 @@ require_relative '../classes/book'
 require_relative '../classes/student'
 require_relative '../classes/teacher'
 require_relative '../classes/rental'
+require_relative './filefunctions'
 require 'json'
 
 module AppFunctions
+  include FileFunctions
   def initialize
     @classroom = Classroom.new('Best')
   end
@@ -80,46 +82,5 @@ module AppFunctions
 
   def empty_string?(obj)
     puts 'Invalid selection' if obj == ''
-  end
-
-  def save_data
-    people_list = @people_list.each_with_index.map do |people, index|
-      {'class' => people.class, 'age' => people.age, 'specialization' => (people.specialization if people.class.to_s == 'Teacher'), 'parent_permission' => people.parent_permission, 'index' => index}
-    end
-      File.write('people_list.json', JSON.generate(people_list))
-    book_list = @book_list.each_with_index.map do |book, index|
-      {'title' => book.title, 'author' => book.author, 'index' => index}
-    end
-    File.write('book_list.json', JSON.generate(book_list))
-    rental_list = @rental_list.map do |rental|
-      {'date' => rental.date, 'book-index' => @book_list.index(rental.book) , 'person-index' => @people_list.index(rental.person)}
-    end
-    File.write('rental_list.json', JSON.generate(rental_list))
-  end
-
-  def load_people
-    return [] unless File.exist?('people_list.json')
-
-    raw_people_list = File.read('people_list.json')
-    JSON.parse(raw_people_list)
-  end
-
-  def load_books
-    return [] unless File.exist?('book_list.json')
-
-    raw_book_list = File.read('book_list.json')
-    raw_book_list = JSON.parse(raw_book_list)
-    arr = []
-    raw_book_list.each do |book|
-      arr.push(Book.new(book["title"], book["author"]))
-    end
-    arr
-  end
-
-  def load_rentals
-    return [] unless File.exist?('rental_list.json')
-
-    raw_rental_list = File.read('rental_list.json')
-    JSON.parse(raw_rental_list)
   end
 end
